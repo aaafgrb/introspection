e8bd871cc963def8de8023a48b0d391fd2425e14
aaafgrb <lucasfuku77@gmail.com>
2025-02-06 17:23:58 -0300

    add custom function page sidebar configuration input

--- src/components/CustomFunctionSidebar.vue
+++ src/components/CustomFunctionSidebar.vue
@@ -5,7 +5,18 @@
       <span v-else>&gt;</span>
     </div>
     <div class="sidebar-content" v-if="!isRetracted">
-      {{ name }}
+      <div class="config-item">
+        <label for="pageTitle">Page Name:</label>
+        <input id="pageTitle" type="text" :value="name">
+        <button>Save</button>
+      </div>
+
+      <div class="config-item function-config">
+        <label for="pageTitle">Function Name:</label>
+        <input id="pageTitle" type="text" :value="customFunctionName">
+        <button>Load</button>
+        <button>Save</button>
+      </div>
     </div>
   </div>
 </template>
@@ -43,10 +54,10 @@ export default {
 
 <style scoped>
 .sidebar {
-  width: 200px;
+  width: 250px;
   position: absolute;
   top: 0;
-  left: -215px;
+  left: -265px;
   bottom: 0;
   background: #f0f4f8;
   color: #333;
@@ -55,6 +66,10 @@ export default {
   display: flex;
   flex-direction: column;
   box-shadow: 4px 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.sidebar-content {
+  overflow-y: scroll;
 }
 
 .is-retracted {
@@ -79,5 +94,56 @@ export default {
   position: absolute;
   border-top-left-radius: 4px;
   border-bottom-left-radius: 4px;
+}
+
+
+/* General styles for the container */
+.config-item {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 5px;
+  padding: 0.5rem;
+  background-color: #f9f9f9;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+label {
+  font-size: 1rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  color: #333;
+}
+
+input[type="text"] {
+  padding: 0.5rem;
+  font-size: 1rem;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  background-color: #fff;
+  color: #333;
+  transition: border 0.3s ease;
+}
+
+input[type="text"]:focus {
+  border-color: #0056b3;
+  outline: none;
+}
+
+button {
+  padding: 0.75rem 1.5rem;
+  font-size: 1rem;
+  border: none;
+  background-color: #608dff;
+  color: #fff;
+  cursor: pointer;
+  border-radius: 5px;
+  margin-top: 5px;
+  transition: background-color 0.3s ease;
+  width: 100%;
+}
+
+button:hover {
+  background-color: #4177ff;
 }
 </style>
